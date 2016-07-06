7d24755aedb30042763e5bf259ef5cd4e7a590fc
Butters <butters@mail.ru>
2016-07-06 23:29:20 +0300

    Basic features have been created

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParams)
+        {
+            kernel = kernelParams;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 }
+            });
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
